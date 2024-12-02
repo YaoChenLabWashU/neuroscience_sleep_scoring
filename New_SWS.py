91a1ac5f840a4434587f9f0c90e10b798a72a66a
ltilden <e.tilden@wustl.edu>
2024-12-02 10:49:35 -0600

    error fixing

--- New_SWS.py
+++ New_SWS.py
@@ -230,7 +230,7 @@ def start_swscoring(d):
 		nearest_epoch = math.floor(seg_len/d['epochlen'])
 		new_length = int(nearest_epoch*d['epochlen']*d['fsd'])
 		this_eeg = this_eeg[0:new_length]
-		normVal = np.load(os.path.join(d['savedir'], d['basename']+'_normVal.npy'))
+		normVal = np.load(os.path.join(eeg_dir, d['basename']+'_normVal.npy'))
 		FeatureDict = SWS_utils.build_feature_dict(this_eeg, d['fsd'], d['epochlen'], this_emg = this_emg,
 			normVal = normVal)
 		this_video, v, this_motion = SWS_utils.initialize_vid_and_move(d, a, EEG_datetime, acq_len, this_eeg)
